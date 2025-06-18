be8e1e8fb763b95a487005126f6cc0624126d934
svpontes <svpontes2015@gmail.com>
2025-06-18 19:53:24 +0100

    error message fix

--- copy-files.js
+++ copy-files.js
@@ -6,8 +6,8 @@ const destination = path.join(__dirname, 'dist/searchResult.html');
 
 fs.copyFile(source, destination, (err) => {
   if (err) {
-    console.error('❌ Erro ao copiar searchResult.html:', err);
+    console.error('Error to copy searchResult.html:', err);
   } else {
-    console.log('✅ searchResult.html copiado para dist/');
+    console.log('searchResult.html copy to dist/');
   }
 });
